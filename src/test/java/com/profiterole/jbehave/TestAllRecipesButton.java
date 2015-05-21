742b1f406af4b30633222985281c5ca104502fa3
severskaya <severskaya8@gmail.com>
2015-05-21 16:41:51 +0300

    + Make different Storys for all situations still some tests are fail, but work on them in progress

--- src/test/java/com/profiterole/jbehave/TestAllRecipesButton.java
+++ src/test/java/com/profiterole/jbehave/TestAllRecipesButton.java
@@ -8,7 +8,7 @@ import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
-public class TestAlrecipesButton {
+public class TestAllRecipesButton {
 
     @Steps
     private ProfiteroleHomeSteps profiteroleHomeSteps;
